4c8f8a360a644c468229b024bad8bc983f9d884f
Javi Santana <qualopec@gmail.com>
2021-12-07 19:11:45 +0100

    polish

--- execute.sh
+++ execute.sh
@@ -20,13 +20,13 @@ tb sql "select * from visits_joined"
 
 
 echo ''
-read -n 1 -p "-> Inserting now the origin for user-00 that arrives late $old_dt"
+read -n 1 -p "-> Inserting now the origin for user-00 that arrives late $old_dt (press enter to continue)"
 tb datasource append ds_origins /tmp/origin_first.csv 1>/dev/null
 tb sql "select * from visits_joined"
 echo '* The country is updated with the old information'
 
 echo ''
-read -n 1 -p '-> Inserting now a new origin for user-01'
+read -n 1 -p '-> Inserting now a new origin for user-01 (press enter)'
 dt=$(date '+%Y/%m/%d %H:%M:%S')
 echo "$dt,'user-01','UK'" > /tmp/tmp.csv
 tb datasource append ds_origins /tmp/tmp.csv 1>/dev/null
@@ -34,7 +34,7 @@ tb sql "select * from visits_joined"
 echo "* It does not add any new visits because the actual visit didn't arrive"
 
 echo ''
-read -n 1 -p '-> And now the visit for user-01 that arrives after the origin we just inserted'
+read -n 1 -p '-> And now the visit for user-01 that arrives after the origin we just inserted (press enter)'
 
 dt=$(date '+%Y/%m/%d %H:%M:%S')
 echo "$dt,'user-01','en-US'" > /tmp/tmp.csv
